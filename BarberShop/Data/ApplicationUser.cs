7e8b85e0a914259a6176408b34750d366b6a7c87
Felix R. Benzan <felixbenz07@gmail.com>
2026-01-15 18:57:58 -0400

    Se incluye el proyecto a desarrollar

---
+++ BarberShop/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BarberShop.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
